2f5b45d623379e895423f531ae5de0dff657b2ee
Greg Godkin <grgodkin@yahoo.com>
2026-09-19 14:10:11 -0600

    Move DS18B20 enable setting to NVS

--- temperature.h
+++ temperature.h
@@ -2,8 +2,8 @@
 
 #include <Arduino.h>
 
-// Set true and install OneWire + DallasTemperature to enable an optional DS18B20.
-constexpr bool DS18B20_ENABLED = false;
+// DS18B20 support is compiled in; enable/disable is stored in NVS.
+constexpr bool DS18B20_ENABLED = true;
 constexpr int DS18B20_PIN = 4;
 constexpr unsigned long DS18B20_INTERVAL_MS = 30000;
 
