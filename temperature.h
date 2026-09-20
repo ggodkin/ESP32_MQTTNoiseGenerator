1238480820124da8b259778f5242280c4942cdf0
Greg Godkin <grgodkin@yahoo.com>
2026-09-20 01:33:22 -0600

    Make temperature interval configurable

--- temperature.h
+++ temperature.h
@@ -5,7 +5,7 @@
 // DS18B20 support is compiled in; enable/disable is stored in NVS.
 constexpr bool DS18B20_ENABLED = true;
 constexpr int DS18B20_PIN = 4;
-constexpr unsigned long DS18B20_INTERVAL_MS = 30000;
+constexpr unsigned long DS18B20_DEFAULT_INTERVAL_MS = 30000;\nconstexpr unsigned long DS18B20_MIN_INTERVAL_MS = 1000;\nconstexpr unsigned long DS18B20_MAX_INTERVAL_MS = 3600000;
 
 void temperatureSetup();
 void temperatureLoop();
